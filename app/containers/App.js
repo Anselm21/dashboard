f6bc635e29ee8a8f0d0a3a91a087502954629bea
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-07-03 14:55:03 +0300

    get cluster info with GET

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,9 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 import Routes from '../routes';
 import {connect} from 'react-redux';
-import { connectWebsocketEventsListener } from '../helpers/EventListener';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import { ACTIONS } from '../redux/reducers/events/actions';
+import request from 'superagent';
+const config = require('../../config.json');
+import _ from 'lodash';
+
 
 @connect(
     state => ({
@@ -20,8 +24,57 @@ export default class App extends Component {
         store: React.PropTypes.object
     };
 
+    constructor(...params) {
+        super(...params);
+        this.state = {
+            serversData: [],
+        };
+        this.getClusterInfo = this.getClusterInfo.bind(this);
+        this.getServerInfo = this.getServerInfo.bind(this);
+    }
+
     componentDidMount() {
-        connectWebsocketEventsListener(this.context.store);
+        this.getClusterInfo();
+    }
+
+    getServerInfo(url) {
+        request
+            .get(url)
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                if (err || !res.ok) {
+                    console.log('error ', err);
+                    this.setState({
+                        serversData: [...this.state.serversData, false]
+                    });
+                } else {
+                    console.log('got data ', res.body);
+                    this.setState({
+                        serversData: [...this.state.serversData, res.body]
+                    });
+                }
+                this.calcClusterStats();
+            });
+    }
+
+    getClusterInfo() {
+        console.log(config);
+        _.forOwn(config.clusters_list, (value, key) => {
+            console.log(key);
+            console.log(value);
+            _.forEach(value.servers, (url) => {
+                console.log(url);
+                this.getServerInfo(url);
+            });
+        });
+        setTimeout(this.getClusterInfo, 3000);
+    }
+
+    calcClusterStats() {
+
+
+
+
     }
 
     render() {
--- app/helpers/EventListener.js
+++ app/helpers/EventListener.js
@@ -1,51 +1,30 @@
 import { ACTIONS } from '../redux/reducers/events/actions';
 import request from 'superagent';
+const config = require('../../config.json');
+import _ from 'lodash';
 
 
 export function connectWebsocketEventsListener(store) {
-    request
-        .get('http://localhost:5000/info')
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-            if (err || !res.ok) {
-                console.log('error ', err);
-            } else {
-                console.log('yay got ', JSON.stringify(res.body));
-            }
+    console.log(config);
+    _.forOwn(config.clusters_list, (value, key) => {
+        console.log(key);
+        console.log(value);
+        _.forEach(value.servers, (url) => {
+            console.log(url);
+            getServerInfo(url);
         });
-    console.log('connecting...');
-    const url = 'http://localhost:5000/';
-    const socket = require('socket.io-client')('http://localhost:5000');
-    socket.on('connect', ()=>{
-        console.log('connect');
-    });
-    socket.on('event', (data)=>{
-        console.log('event ', data);
-    });
-    socket.on('message', (data)=>{
-        // console.log('message ', data);
-        if (data.server_name) {
-            store.dispatch({
-                type: ACTIONS.NEW_EVENT,
-                topic: data.server_name,
-                event: data
-            });
-        }
-    });
-    socket.on('disconnect', ()=>{
-        console.log('disconnect');
     });
 }
 
-export function getServersInfo(store) {
+export function getServerInfo(url) {
     request
-        .get('http://localhost:5000/info')
+        .get(url)
         .set('Accept', 'application/json')
         .end((err, res) => {
             if (err || !res.ok) {
                 console.log('error ', err);
             } else {
-                console.log('yay got ', JSON.stringify(res.body));
+                console.log('got data ', JSON.stringify(res.body));
             }
         });
 }
